f2433493529ff45fd3b91a0a88062904d8263a90
Deyvison Penha <deyvisonpenha1@gmail.com>
2020-05-03 01:07:06 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
 
+import api from "./services/api";
+
 function App() {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setProjects(response.data);
+    })
+  }, [projects]);
+
   async function handleAddRepository() {
-    // TODO
+    api.post('repositories',{
+      title: `novo projeto ${Date.now()}`,
+    }).then(response => {
+      setProjects([...projects,response.data]);
+    })
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {projects.map(project =>
+          <li key={project.id}>
+            {project.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
+            </button>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+//http://localhost:3333
+
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://3333-d812621d-7013-44b8-af09-2cebfbf3cb9d.ws-us02.gitpod.io",
 });
 
 export default api;
